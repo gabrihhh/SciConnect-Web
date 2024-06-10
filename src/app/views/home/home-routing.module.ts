3cb6ffc2504e4669e5cea6a5323f08d166628890
Gabriel Patara <gabriel.patara@gmail.com>
2024-06-10 10:20:55 -0300

    rotas filhas da home

--- src/app/views/home/home-routing.module.ts
+++ src/app/views/home/home-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { PublishComponent } from '../publish/publish.component';
 ;
 
 const routes: Routes = [
-    {path:'',component:HomeComponent}
+    {path:'',component:HomeComponent,children:[
+      {path:'publish', component: PublishComponent}
+    ]},
 ];
 
 @NgModule({
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { UserService } from 'src/app/shared/services/storage/user/user.service'
 
 @Component({
@@ -7,5 +8,9 @@ import { UserService } from 'src/app/shared/services/storage/user/user.service'
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent{
+  constructor(private router:Router,private route: ActivatedRoute){}
 
+  public navigatingToPublish(){
+    this.router.navigate(['publish'], { relativeTo: this.route });
+  }
 }
