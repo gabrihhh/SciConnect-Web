806abd71304452d6bdac0e62f3c93102f46acac4
Gabriel Patara <gabriel.patara@gmail.com>
2024-06-10 11:07:06 -0300

    input funcionais da publish

--- src/app/views/publish/publish.component.ts
+++ src/app/views/publish/publish.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-publish',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./publish.component.scss']
 })
 export class PublishComponent {
+  @ViewChild('inputTitulo') inputTitulo!: ElementRef
+  @ViewChild('inputDiciplina') inputDiciplina!: ElementRef
+  @ViewChild('inputTexto') inputTexto!: ElementRef
 
+  public post(){
+    console.log(this.inputTitulo.nativeElement.value,this.inputDiciplina.nativeElement.value,this.inputTexto.nativeElement.value)
+  }
 }
