dd60255950f1810f441fd6a01718765479062e8a
Gabriel Patara <gabriel.patara@gmail.com>
2024-06-13 15:12:00 -0300

    perfil feito

--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IPost, IUser } from 'src/app/shared/interface/user.interface';
 
+interface IUserPost {
+  titulo:'teste teste',
+  data:'02/05/2000',
+  describe:'Curabitur molestie odio diam, non sagittis turpis pulvinar ac. Nullam fringilla non velit vel iaculis. Curabitur neque lorem, ornare ut purus non, faucibus fringilla nulla. Integer mauris metus, finibus sed sapien non, gravida tincidunt mi. Phasellus laoreet ligula vel libero venenatis, eu tristique dui varius. Quisque in nunc vitae metus viverra dignissim.'
+}
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -11,14 +17,24 @@ export class ProfileComponent implements OnInit{
   public user:IUser | undefined; 
   public seguidores:number = 1234;
   public seguindo:number = 4321;
-  public postsOfUser = [] 
+  public postsOfUser:IUserPost[] = [] 
+  
   constructor(private route: ActivatedRoute){}
 
   ngOnInit(): void {
+    this.getUserPost()
     this.route.queryParams.subscribe(params => {
       this.user = JSON.parse(params['parametro']);
-      console.log(this.user)
     });
+  }
 
+  public getUserPost(){
+    for(let i = 0;i<8;i++){
+      this.postsOfUser.push({
+        titulo:'teste teste',
+        data:'02/05/2000',
+        describe:'Curabitur molestie odio diam, non sagittis turpis pulvinar ac. Nullam fringilla non velit vel iaculis. Curabitur neque lorem, ornare ut purus non, faucibus fringilla nulla. Integer mauris metus, finibus sed sapien non, gravida tincidunt mi. Phasellus laoreet ligula vel libero venenatis, eu tristique dui varius. Quisque in nunc vitae metus viverra dignissim.'
+      })
+    }
   }
 }
